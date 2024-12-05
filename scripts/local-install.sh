53ee20bf764a00ee32199b6ee7ac3488a7232fd7
Dan Blumenfeld <dbld@tbdvox.com>
2024-12-05 15:22:47 -0500

    Added local preservation of user-supplied values

--- scripts/local-install.sh
+++ scripts/local-install.sh
@@ -4,16 +4,29 @@
 #
 # Example: local-install.sh
 
+CONFIG_FILE="local-install.config.env"
+
+# Check if config file exists
+if [ -f "$CONFIG_FILE" ]; then
+    # Load saved variables
+    source "$CONFIG_FILE"
+else
+    # Collect information and save to config file
+    echo "Input Ubuntu user name:"
+    read user
+    echo "Input name you wish to use in Git:"
+    read git_user_name
+    echo "Input email you wish to use in Git:"
+    read git_user_email
+
+    # Save variables to config file
+    echo "user=\"$user\"" > "$CONFIG_FILE"
+    echo "git_user_name=\"$git_user_name\"" >> "$CONFIG_FILE"
+    echo "git_user_email=\"$git_user_email\"" >> "$CONFIG_FILE"
+fi
+
 # Make sure scripts are executable
 find ./ -type f -name "*.sh" -exec chmod +x {} \;
-
-# Collect information
-echo "Input Ubuntu user name:"
-read user
-echo "Input name you wish to use in Git:"
-read git_user_name
-echo "Input email you wish to use in Git:"
-read git_user_email
 
 # Set up Ansible
 ./ansible.install.sh
@@ -26,4 +39,3 @@ read git_user_email
 
 # Install VS Code
 ./vscode.install.sh
-

783f71dee34a2c887f1a4bfbc76bb97d275ac234
Dan Blumenfeld <dbld@tbdvox.com>
2024-12-05 15:27:11 -0500

    Add user prompt for existing config

--- scripts/local-install.sh
+++ scripts/local-install.sh
@@ -4,14 +4,31 @@
 #
 # Example: local-install.sh
 
-CONFIG_FILE="local-install.config.env"
+CONFIG_FILE="config.env"
 
-# Check if config file exists
+# Check if the config file exists
 if [ -f "$CONFIG_FILE" ]; then
-    # Load saved variables
-    source "$CONFIG_FILE"
+    echo "A configuration file ($CONFIG_FILE) already exists. Do you want to use it? (y/n)"
+    read use_existing_config
+    if [[ "$use_existing_config" == "y" ]]; then
+        # Load saved variables
+        source "$CONFIG_FILE"
+    else
+        # Collect new information and overwrite the config file
+        echo "Input Ubuntu user name:"
+        read user
+        echo "Input name you wish to use in Git:"
+        read git_user_name
+        echo "Input email you wish to use in Git:"
+        read git_user_email
+
+        # Save variables to config file
+        echo "user=\"$user\"" > "$CONFIG_FILE"
+        echo "git_user_name=\"$git_user_name\"" >> "$CONFIG_FILE"
+        echo "git_user_email=\"$git_user_email\"" >> "$CONFIG_FILE"
+    fi
 else
-    # Collect information and save to config file
+    # Collect information and create a new config file
     echo "Input Ubuntu user name:"
     read user
     echo "Input name you wish to use in Git:"
